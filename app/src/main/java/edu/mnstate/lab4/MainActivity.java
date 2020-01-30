2836119c6f2999fe916a3eab40dc3e22e7918adb
JohnathanFL <jfl747076@gmail.com>
2020-01-30 17:24:39 -0600

    Lab4 done

--- app/src/main/java/edu/mnstate/lab4/MainActivity.java
+++ app/src/main/java/edu/mnstate/lab4/MainActivity.java
@@ -3,24 +3,41 @@ package edu.mnstate.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView lblGreeting;
     private EditText txtFirstName;
+    private Button btnGreet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lblGreeting = (TextView)findViewById(R.id.lblGreeting);
-        txtFirstName = (EditText)findViewById(R.id.txtFirstName);
+        lblGreeting = findViewById(R.id.lblGreeting);
+        txtFirstName = findViewById(R.id.txtFirstName);
+        btnGreet = findViewById(R.id.btnGreet);
+
+        /*btnGreet.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String tmp = getResources().getString(R.string.hello_there);
+                lblGreeting.setText(tmp + " " + txtFirstName.getText());
+            }
+
+        });*/
+        btnGreet.setOnClickListener(v -> {
+            String tmp = getResources().getString(R.string.hello_there);
+            lblGreeting.setText(tmp + " " + txtFirstName.getText());
+        });
+
     }
 
     public void showGreeting(View v) {
-        String tmp = getResources().getString(R.string.hello_there);
-        lblGreeting.setText(tmp + " " + txtFirstName.getText());
+//        String tmp = getResources().getString(R.string.hello_there);
+//        lblGreeting.setText(tmp + " " + txtFirstName.getText());
     }
 }
