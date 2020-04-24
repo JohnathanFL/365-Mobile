ead00f8b2aa12607f7e10ea607982e412f7cdcd3
JohnathanFL <jfl747076@gmail.com>
2020-04-24 15:20:36 -0500

    Add license info and required headers. Double check for hardcoded stuff

--- app/src/main/java/edu/mnstate/jz1652ve/assign6/MainActivity.java
+++ app/src/main/java/edu/mnstate/jz1652ve/assign6/MainActivity.java
@@ -1,3 +1,24 @@
+/**
+ * Author: Johnathan Lee
+ * Class: CSIS365 Mobile, MSUM
+ * Due: 04/24/20
+ * Assign 6 - A simple Github Jobs client that searches jobs and lets you open them in a browser.
+
+ Copyright 2020 Johnathan Lee
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package edu.mnstate.jz1652ve.assign6;
 
 import androidx.appcompat.app.AppCompatActivity;
