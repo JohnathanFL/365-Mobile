44bb5403dc48268eded3fa564a32909e582bf7c2
JohnathanFL <jfl747076@gmail.com>
2020-05-07 21:49:42 -0500

    Add headers

--- app/src/main/java/edu/mnstate/jz1652ve/finalproject/ContactListFragment.kt
+++ app/src/main/java/edu/mnstate/jz1652ve/finalproject/ContactListFragment.kt
@@ -1,3 +1,12 @@
+/**
+ * A "Sphere of Influence" application that stores contacts and tells you how much pull ya got.
+ * Author: Johnathan Lee
+ * Date: 05/08/2020
+ * MSUM CSIS-365 Mobile, Spring 2020
+ *
+ *
+ * This project is under the public domain.
+ */
 package edu.mnstate.jz1652ve.finalproject
 
 import android.os.Bundle
